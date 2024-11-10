90d7c4e0cbed640cf543a0458d195ed5278ab4c1
JaeYoung290 <2159wodud@gmail.com>
2024-11-10 23:49:56 +0900

    3차 실습

--- assignment_week_4/app/build.gradle.kts
+++ assignment_week_4/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.assignment_week_02"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.assignment_week_02"
